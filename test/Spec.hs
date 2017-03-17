71dd514103ee554203836344ba60b3ef9ec5db4d
Richard Lupton <richard.lupton@gmail.com>
2017-03-17 20:47:48 +0000

    Copy over code
    Copy over code from VPN, and add a basic test to ensure a stack actually runs.

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,4 +1,27 @@
+import Test.Framework as TF
+import Test.Framework.Providers.HUnit (hUnitTestToTests)
+import Test.HUnit as HU
+import Test.HUnit ((~:),(@=?))
+
 import Protolude
+import Control.Concurrent.Stack
 
 main :: IO ()
-main = putStrLn ("Test suite not yet implemented" :: Text)
+main = defaultMain tests
+
+tests :: [ TF.Test ]
+tests = [ basicTests ]
+
+basicTests :: TF.Test
+basicTests = testGroup "Basic tests" . hUnitTestToTests $
+  HU.TestList [ canRunBasicStack ]
+
+canRunBasicStack :: HU.Test
+canRunBasicStack = "Check a basic Stack runs" ~:
+  runStack stack
+  where
+    stack :: Stack ()
+    stack = do
+      register $ return ()
+      register $ return ()
+      register $ return ()
